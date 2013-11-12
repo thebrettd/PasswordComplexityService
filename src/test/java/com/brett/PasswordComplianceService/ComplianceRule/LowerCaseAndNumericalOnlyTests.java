203421e835d8fc39af1a6aaefda8ec1d1f446b93
thebrettd <Btd123!>
2013-11-12 14:51:09 -0800

    Use @Before in test

--- src/test/java/com/brett/PasswordComplianceService/ComplianceRule/LowerCaseAndNumericalOnlyTests.java
+++ src/test/java/com/brett/PasswordComplianceService/ComplianceRule/LowerCaseAndNumericalOnlyTests.java
@@ -1,6 +1,7 @@
 package com.brett.PasswordComplianceService.ComplianceRule;
 
 import com.brett.PasswordComplianceService.Password;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.Errors;
@@ -9,13 +10,20 @@ import static org.junit.Assert.*;
 
 public class LowerCaseAndNumericalOnlyTests {
 
+    Password passwordToTest;
+    LowerCaseAndNumericalOnly validator;
+    Errors errors;
+
+    @Before
+    public void initialize(){
+        passwordToTest = new Password();
+        validator = new LowerCaseAndNumericalOnly();
+        errors = new BeanPropertyBindingResult(passwordToTest, "passwordToTest");
+    }
+
     @Test
     public void testNullPasswordInvalid() throws Exception {
-        Password passwordToTest = new Password();
 
-        LowerCaseAndNumericalOnly validator = new LowerCaseAndNumericalOnly();
-
-        Errors errors = new BeanPropertyBindingResult(passwordToTest, "passwordToTest");
         validator.validate(passwordToTest, errors);
 
         assertTrue(errors.hasErrors());
@@ -24,12 +32,7 @@ public class LowerCaseAndNumericalOnlyTests {
 
     @Test
     public void testWhitespacePasswordInvalid() throws Exception {
-        Password passwordToTest = new Password();
         passwordToTest.setMyPassword(" ");
-
-        LowerCaseAndNumericalOnly validator = new LowerCaseAndNumericalOnly();
-
-        Errors errors = new BeanPropertyBindingResult(passwordToTest, "passwordToTest");
         validator.validate(passwordToTest, errors);
 
         assertTrue(errors.hasErrors());
@@ -38,11 +41,6 @@ public class LowerCaseAndNumericalOnlyTests {
 
     @Test
     public void testNoNumbersInvalid() throws Exception{
-        Password passwordToTest = new Password();
-        passwordToTest.setMyPassword("abc");
-        LowerCaseAndNumericalOnly validator = new LowerCaseAndNumericalOnly();
-
-        Errors errors = new BeanPropertyBindingResult(passwordToTest, "passwordToTest");
         validator.validate(passwordToTest, errors);
 
         assertTrue(errors.hasErrors());
@@ -51,11 +49,7 @@ public class LowerCaseAndNumericalOnlyTests {
 
     @Test
     public void testNoLowercaseLettersInvalid() throws Exception{
-        Password passwordToTest = new Password();
         passwordToTest.setMyPassword("123");
-        LowerCaseAndNumericalOnly validator = new LowerCaseAndNumericalOnly();
-
-        Errors errors = new BeanPropertyBindingResult(passwordToTest, "passwordToTest");
         validator.validate(passwordToTest, errors);
 
         assertTrue(errors.hasErrors());
@@ -64,11 +58,7 @@ public class LowerCaseAndNumericalOnlyTests {
 
     @Test
     public void testOnlyUppercaseInvalid() throws Exception{
-        Password passwordToTest = new Password();
         passwordToTest.setMyPassword("ABC123");
-        LowerCaseAndNumericalOnly validator = new LowerCaseAndNumericalOnly();
-
-        Errors errors = new BeanPropertyBindingResult(passwordToTest, "passwordToTest");
         validator.validate(passwordToTest, errors);
 
         assertTrue(errors.hasErrors());
@@ -77,11 +67,7 @@ public class LowerCaseAndNumericalOnlyTests {
 
     @Test
     public void testBasicValid() throws Exception{
-        Password passwordToTest = new Password();
         passwordToTest.setMyPassword("abc123");
-        LowerCaseAndNumericalOnly validator = new LowerCaseAndNumericalOnly();
-
-        Errors errors = new BeanPropertyBindingResult(passwordToTest, "passwordToTest");
         validator.validate(passwordToTest, errors);
 
         assertFalse(errors.hasErrors());
@@ -90,18 +76,10 @@ public class LowerCaseAndNumericalOnlyTests {
 
     @Test
     public void testCombinationInvalid() throws Exception{
-        Password passwordToTest = new Password();
         passwordToTest.setMyPassword("abcABC123");
-        LowerCaseAndNumericalOnly validator = new LowerCaseAndNumericalOnly();
-
-        Errors errors = new BeanPropertyBindingResult(passwordToTest, "passwordToTest");
         validator.validate(passwordToTest, errors);
 
         assertTrue(errors.hasErrors());
         assertNotNull(errors.getFieldError("myPassword"));
     }
-
-
-
-
 }
